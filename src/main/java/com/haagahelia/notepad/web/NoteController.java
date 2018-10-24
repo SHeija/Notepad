fbd3b3c56271f09b241a0df72fa79cb2d4102996
SHeija <sirkku.heija@myy.haaga-helia.fi>
2018-10-24 20:01:03 +0300

    added add and delete functions

--- src/main/java/com/haagahelia/notepad/web/NoteController.java
+++ src/main/java/com/haagahelia/notepad/web/NoteController.java
@@ -3,8 +3,10 @@ package com.haagahelia.notepad.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,12 +33,43 @@ public class NoteController {
 		return "notelist";
 	}
 	
+	//add note
+	@RequestMapping("/add")
+	public String noteAdd(Model model) {
+		model.addAttribute("note", new Note());
+		return "addnote";
+	}
+	
+	//save note
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String noteSave(Note note) {
+		noteRepo.save(note);
+		return "redirect:notelist";
+	}
+	
+	//delete via html
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String noteDelete(@PathVariable("id") Long id) {
+		noteRepo.deleteById(id);
+		return "redirect:../notelist";
+	}
+	
+	
 	//REST API
 	
-	//get all notes TEST!
+	//get all notes 
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value = "/notes", method=RequestMethod.GET)
 	public @ResponseBody List<Note> noteListRest(){
 		return (List<Note>) noteRepo.findAll();
+	}
+	
+	//delete via REST
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@RequestMapping(value = "/note/{id}", method=RequestMethod.DELETE)
+	public String noteDeleteRest(@PathVariable("id") Long id) {
+		noteRepo.deleteById(id);
+		return "redirect:../notelist";
 	}
 
 }
